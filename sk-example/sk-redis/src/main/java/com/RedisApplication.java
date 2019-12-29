7cbc2ac5a38e33c7fdb0e7a89d023573c36b6d81
salted-sk <731109627@qq.com>
2019-12-29 19:15:43 +0800

    配置文件key长度问题修改

---
+++ sk-example/sk-redis/src/main/java/com/RedisApplication.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *暂定redis启动类
+ *
+ * @author zhangqiao
+ * @since 2019/12/24 14:16
+ */
+@SpringBootApplication
+public class RedisApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RedisApplication.class, args);
+	}
+}
