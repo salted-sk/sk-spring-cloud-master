416637e2ec73817d8728f85092e812a28aac232d
salted-sk <731109627@qq.com>
2020-01-06 22:53:23 +0800

    添加服务链路追踪sleuth

--- sk-spring-cloud-project/spring-cloud-sleuth/src/main/java/com/sk/SleuthApplication.java
+++ sk-spring-cloud-project/spring-cloud-sleuth/src/main/java/com/sk/SleuthApplication.java
@@ -2,7 +2,6 @@ package com.sk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.internal.EnableZipkinServer;
 
 /**
  * sleuth启动类
@@ -11,7 +10,6 @@ import zipkin.server.internal.EnableZipkinServer;
  * @date 2020/1/6
  */
 @SpringBootApplication
-@EnableZipkinServer
 public class SleuthApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SleuthApplication.class, args);
