db8c1a9060afdda55b2fb00ccd44f01500a7dbef
sk <731109627@qq.com>
2020-01-16 11:04:20 +0800

    zipkin相關配置

--- sk-spring-cloud-project/spring-cloud-gateway/src/main/java/com/sk/GatewayApplication.java
+++ sk-spring-cloud-project/spring-cloud-gateway/src/main/java/com/sk/GatewayApplication.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import java.net.URI;
 
 /**
  * 微服务网关
@@ -14,10 +22,18 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
+@Component
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> routerFunction() {
+        return RouterFunctions.route(RequestPredicates.GET("/"),
+                request -> ServerResponse.temporaryRedirect(URI.create("index.html")).build()
+                );
     }
 
 }
