93b93e89f432a72b2e14abb1cb4501649588fda0
salted-sk <731109627@qq.com>
2020-02-25 09:56:36 +0800

    redisTemplate使用

--- sk-example/sk-redis/src/main/java/com/RedisApplication.java
+++ sk-example/sk-redis/src/main/java/com/RedisApplication.java
@@ -3,6 +3,8 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  *暂定redis启动类
@@ -15,5 +17,10 @@ import org.springframework.cache.annotation.EnableCaching;
 public class RedisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RedisApplication.class, args);
+	}
+
+	@Bean
+	public RedisTemplate redisTemplate() {
+		return new RedisTemplate();
 	}
 }
---
+++ sk-example/sk-redis/src/main/java/com/sk/service/RedisService.java
@@ -0,0 +1,23 @@
+package com.sk.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * redis测试类
+ *
+ * @author zhangqiao
+ * @since 2020/2/25 9:44
+ */
+@Service
+public class RedisService {
+
+	@Autowired
+	private RedisTemplate redisTemplate;
+
+	public void test() {
+
+	}
+
+}
