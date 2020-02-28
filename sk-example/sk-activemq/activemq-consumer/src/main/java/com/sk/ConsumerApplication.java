bb676eb48e56d6cc1a35952e1ecb285ce1ea5323
salted-sk <731109627@qq.com>
2020-02-28 11:10:14 +0800

    添加activemq模块

---
+++ sk-example/sk-activemq/activemq-consumer/src/main/java/com/sk/ConsumerApplication.java
@@ -0,0 +1,19 @@
+package com.sk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO
+ *
+ * @author zhangqiao
+ * @since 2020/2/28 11:07
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+}
