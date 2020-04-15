eaccf0b8649c74b01797ef080d88ed88bee7a5b9
sk <731109627@qq.com>
2020-04-15 14:18:02 +0800

    新增jpademo模块

---
+++ sk-example/sk-data-jpa/src/main/java/com/sk/DataJpaApplication.java
@@ -0,0 +1,19 @@
+package com.sk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO
+ *
+ * @author zhangqiao
+ * @since 2020/4/15 14:15
+ */
+@SpringBootApplication
+public class DataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaApplication.class, args);
+    }
+
+}
