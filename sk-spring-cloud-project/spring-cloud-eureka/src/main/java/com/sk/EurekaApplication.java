481e4c1fb211af32e2c21ed15ae88b2a48b4d58b
sk <731109627@qq.com>
2020-04-26 19:04:14 +0800

    添加eureka模块

---
+++ sk-spring-cloud-project/spring-cloud-eureka/src/main/java/com/sk/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.sk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * TODO
+ *
+ * @author zhangqiao
+ * @since 2020/4/26 17:58
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
