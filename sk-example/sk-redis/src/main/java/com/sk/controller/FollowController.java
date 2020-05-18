1f99c7881c9aadce5e85fa2a27c61bf3e666e3f8
sk <731109627@qq.com>
2020-05-18 15:31:13 +0800

    添加用户关注功能

---
+++ sk-example/sk-redis/src/main/java/com/sk/controller/FollowController.java
@@ -0,0 +1,10 @@
+package com.sk.controller;
+
+/**
+ * 用户关注控制层
+ *
+ * @author zhangqiao
+ * @since 2020/5/18 15:30
+ */
+public class FollowController {
+}
---
+++ sk-example/sk-redis/src/main/java/com/sk/service/FollowService.java
@@ -0,0 +1,13 @@
+package com.sk.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 用户关注
+ *
+ * @author zhangqiao
+ * @since 2020/5/18 15:30
+ */
+@Service
+public class FollowService {
+}
